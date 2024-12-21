94abc9582e4b985949c65266adcf19377baf1053
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-22 00:33:29 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -349,8 +349,8 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:                               
-                cc = f'**[🎞️] Vid_ID :** {str(count).zfill(3)}\n\n**Video Title :** {name1}( @ANKIT_SHAKYA73 )({res}).mkv\n\n**Batch Name :** {b_name}\n\n**Extracted By ➤ {CR}\n\n**Bot By ➤ ᴀɴᴋɪᴛ sʜᴀᴋʏᴀ'
-                cc1 = f'**[📄] Pdf_ID :** {str(count).zfill(3)}\n\n**File Title :** {name1}( @ANKIT_SHAKYA73 ).pdf\n\n**Batch Name :** {b_name}\n\n**Extracted By ➤ {CR}\n\n**Bot By ➤ ᴀɴᴋɪᴛ sʜᴀᴋʏᴀ'
+                cc = f'**[🎞️] Vid_ID :** {str(count).zfill(3)}\n\n**Video Title :** {name1}( @ANKIT_SHAKYA73 ){res}.mkv\n\n**Batch Name :** {b_name}\n\n**Extracted By ➤ {CR}\n\n**𝐁𝐨𝐭 𝐁𝐲 ➤ ᴀɴᴋɪᴛ sʜᴀᴋʏᴀ'
+                cc1 = f'**[📄] Pdf_ID :** {str(count).zfill(3)}\n\n**File Title :** {name1}( @ANKIT_SHAKYA73 ).pdf\n\n**Batch Name :** {b_name}\n\n**Extracted By ➤ {CR}\n\n**𝐁𝐨𝐭 𝐁𝐲 ➤ ᴀɴᴋɪᴛ sʜᴀᴋʏᴀ'
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
