b6fef78987dae17a0c2f32890ffb871ac840e6cc
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-30 18:57:03 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -35,7 +35,7 @@ bot = Client(
 )
 
 
-cookies_file_path = os.getenv("COOKIES_FILE_PATH", "youtube_cookies.txt")
+cookies_file_path = os.getenv("COOKIES_FILE_PATH", "cookies.txt")
 
 # Define aiohttp routes
 routes = web.RouteTableDef()
